1f56f9368db460dda2cc44252488605e6a686db1
Dimagithub <dima@semensky.com>
2021-02-11 16:04:24 -0500

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -24,7 +24,7 @@ const Header = ({ siteTitle }) => (
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
+          {siteTitle} - DIma test
         </Link>
       </h1>
     </div>
